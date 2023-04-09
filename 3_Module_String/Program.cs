07de0a781dcf4d7a288462820d3b9ff61a7beae1
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-04-09 09:47:22 +0300

    Добавьте файлы проекта.

---
+++ 3_Module_String/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3_Module_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Bogdan";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
