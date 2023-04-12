80a59e3d05c1a1840671d8418e13166a13717093
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-04-12 09:14:07 +0300

    3.3.2

--- 3_Module_String/Program.cs
+++ 3_Module_String/Program.cs
@@ -6,8 +6,16 @@ namespace _3_Module_String
     {
         static void Main(string[] args)
         {
-            string MyName = "Bogdan";
-            Console.WriteLine(MyName);
+            string Name = "Bogdan";
+            byte Age = 21;
+            bool Pet = false;
+            double ShoeSize = 43.5;
+            Console.WriteLine("Hellow World.");
+            Console.WriteLine("\t My name is "+ Name);
+            Console.WriteLine("\t My age is "+ Age);
+            Console.WriteLine("\n\t Do you have a pet ? "+ Pet);
+            Console.WriteLine("\t My shoe size is "+ ShoeSize);
+            Console.WriteLine("\x23");
             Console.ReadKey();
         }
     }
