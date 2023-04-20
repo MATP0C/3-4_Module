3098f278e336b6bee6cd7405a0cf23bfb5e10afc
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-04-20 18:49:26 +0300

    4.5.6

--- 3_Module_String/Program.cs
+++ 3_Module_String/Program.cs
@@ -11,42 +11,45 @@ namespace _3_Module_String
         {
             ( string Name , string LastName,string Login, int LoginLength, bool HasPet, byte Age, string[] favcolors, double ShoeSize, byte Height)User;
 
-            Console.WriteLine("Введите ваше имя:");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите вашу фамилию:");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Есть ли у вас животные? Да или нет");
-            var result = Console.ReadLine();
-            if (result == "Да")
+            for (int k = 0; k < 3; k++)
             {
-                User.HasPet = true;
-            }    
-            else
-            {
-                User.HasPet = false;
-            }
+                Console.WriteLine("Введите ваше имя:");
+                User.Name = Console.ReadLine();
 
-            Console.WriteLine("Введите ваш логин:");
-            User.Login = Console.ReadLine();
-            User.LoginLength = User.Login.Length;
-            Console.WriteLine("Длина вашего логина {0}",User.LoginLength);
+                Console.WriteLine("Введите вашу фамилию:");
+                User.LastName = Console.ReadLine();
 
-            Console.WriteLine("Введите ваш возраст:");
-            User.Age = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("Есть ли у вас животные? Да или нет");
+                var result = Console.ReadLine();
+                if (result == "Да")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
 
-            Console.WriteLine("Введите ваш размер обуви:");
-            User.ShoeSize = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите ваш логин:");
+                User.Login = Console.ReadLine();
+                User.LoginLength = User.Login.Length;
+                Console.WriteLine("Длина вашего логина {0}", User.LoginLength);
 
-            Console.WriteLine("Введите ваш рост:");
-            User.Height = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("Введите ваш возраст:");
+                User.Age = Convert.ToByte(Console.ReadLine());
 
-            User.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета:");
-            for (int i = 0; i < User.favcolors.Length; i++)
-            {
-                User.favcolors[i] = Console.ReadLine();
+                Console.WriteLine("Введите ваш размер обуви:");
+                User.ShoeSize = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите ваш рост:");
+                User.Height = Convert.ToByte(Console.ReadLine());
+
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета:");
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
             }
 
             Console.WriteLine("Write your favorite color in English with a small letter");
